b9849f6f8ab10a923dd89d37cf6619e637d9c34b
Nonato Home <nonato.developer@gmail.com>
2018-08-13 15:12:08 -0300

    overflow in msg textFlow

--- src/ponghaukisockets/FXMLDocumentController.java
+++ src/ponghaukisockets/FXMLDocumentController.java
@@ -8,6 +8,10 @@ package ponghaukisockets;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,6 +20,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Circle;
 import javafx.animation.TranslateTransition;
+import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
@@ -62,18 +67,6 @@ public class FXMLDocumentController implements Initializable {
         pieceMap.setPieceYellowA(new Piece(circuloAmareloA, 3));
         pieceMap.setPieceYellowB(new Piece(circuloAmareloB, 4));
         
-   
-                
-        Text text1 = new Text("Mensagem jogador Azul\n");
-        text1.setFill(Paint.valueOf("#1e90ff"));
-        text1.setFont(Font.font("Helvetica", FontPosture.REGULAR, 20));
-        
-        Text text2 = new Text("Mensagem jogador Amarelo\n");
-        text2.setFill(Paint.valueOf("#c3c310"));
-        text2.setFont(Font.font("Helvetica", FontPosture.REGULAR, 20));
-
-        msgTextFlow.getChildren().addAll(text1, text2);
-        
         //Events
         circuloAzulA.setOnMouseClicked((e)->{
             pieceMap.moveBlueA();
@@ -90,9 +83,42 @@ public class FXMLDocumentController implements Initializable {
         circuloAmareloB.setOnMouseClicked((e)->{
             pieceMap.moveYellowB();
         });
+        
+        jfxTf_message.setOnKeyPressed((e)->{
+            if(e.getCode().equals(KeyCode.ENTER)){
+                addMessageBlue(jfxTf_message.getText());
+                jfxTf_message.setText("");
+            }
+        });
+        
+        addMessageBlue("Teste com texto azul");
+        addMessageYellow("Teste com texto amarelo");
     }   
     
     public void circleClick(){
         
     }
+    
+    public void addMessageBlue(String msg){
+        DateFormat df = new SimpleDateFormat("HH:mm:ss");
+        Text dth = new Text(df.format(new Date()).toString() +" -- ");
+        dth.setFont(Font.font("Helvetica", FontPosture.REGULAR, 16));
+        
+        Text text1 = new Text(msg+"\n");
+        text1.setFill(Paint.valueOf("#1e90ff"));
+        text1.setFont(Font.font("Helvetica", FontPosture.REGULAR, 20));
+        msgTextFlow.getChildren().addAll(dth, text1);
+    }
+    
+    public void addMessageYellow(String msg){
+        DateFormat df = new SimpleDateFormat("HH:mm:ss");
+        Text dth = new Text(df.format(new Date()).toString() +" -- ");
+        dth.setFont(Font.font("Helvetica", FontPosture.REGULAR, 16));
+        
+        Text text1 = new Text(msg+"\n");
+        text1.setFill(Paint.valueOf("#c3c310"));
+        text1.setFont(Font.font("Helvetica", FontPosture.REGULAR, 20));
+        msgTextFlow.getChildren().addAll(dth, text1);
+    }
+    
 }
